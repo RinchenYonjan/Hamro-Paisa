2721f3521212cbb6dfd565ee7c2eb977698f3023
Rinchen Yonjan <yonzanrinchen@gmail.com>
2025-01-04 09:01:23 +0545

    First commit

---
+++ App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace HamroPaisa
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+    }
+}
