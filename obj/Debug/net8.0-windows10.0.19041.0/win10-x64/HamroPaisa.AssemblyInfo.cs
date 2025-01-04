8a05fe39f91668935895a918e5e75f5f10978af3
Rinchen Yonjan <yonzanrinchen@gmail.com>
2025-01-04 11:28:55 +0545

    second commit

--- obj/Debug/net8.0-windows10.0.19041.0/win10-x64/HamroPaisa.AssemblyInfo.cs
+++ obj/Debug/net8.0-windows10.0.19041.0/win10-x64/HamroPaisa.AssemblyInfo.cs
@@ -18,7 +18,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HamroPaisa")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+2721f3521212cbb6dfd565ee7c2eb977698f3023")]
 [assembly: System.Reflection.AssemblyProductAttribute("HamroPaisa")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HamroPaisa")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
